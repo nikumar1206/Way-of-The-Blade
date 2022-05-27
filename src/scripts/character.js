ec671136d81b4df3395728048b8837dd7ac09b3b
nikumar1206 <nikumar1206@gmail.com>
2022-05-26 21:15:06 -0400

    improved falling

--- src/scripts/character.js
+++ src/scripts/character.js
@@ -1,3 +1,4 @@
+const GRAVITY = 2
 export class Character {
     constructor(posX, posY, velX = 0, velY = 25) {
         this.posX = posX
@@ -7,17 +8,21 @@ export class Character {
     } 
 
     draw(ctx) {
-        // ctx.fillStyle = "lightblue";
-        // ctx.fillRect(0, 0, 1024, 526);
-    
         ctx.fillStyle = "green"
         ctx.fillRect(this.posX, this.posY, 50, 150)
     }
 
     update(ctx) {
         this.draw(ctx);
+    
         this.posX += this.velX
         this.posY += this.velY
+    
+        if (!(this.posY >= 376)) {
+            this.velY = GRAVITY
+        } else {
+            this.velY = 0
+        }
     }
 
     // animate(ctx) {
